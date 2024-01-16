cedf600053e3dcc348244c2d31f8c81122d5720b
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2024-01-16 13:36:59 +0100

    ver 0.09 - Created webpack

---
+++ app/components/header.jsx
@@ -0,0 +1,1 @@
+const React = require("react");class Header extends React.Component {    constructor(props) {        super(props);    }    render() {        return <h2>{this.props.text}</h2>;    }}module.exports = Header;
