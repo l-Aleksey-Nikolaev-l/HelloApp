76c75d84514debdfa1889e583beaea24899282a3
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2024-01-17 11:12:58 +0100

    ver 0.10 - Created class component with STATE

--- app/components/article.jsx
+++
@@ -1,1 +0,0 @@
-const React = require("react");class Article extends React.Component {    constructor(props) {        super(props);    }    render() {        return <div>{this.props.content}</div>;    }}module.exports = Article;
--- app/components/header.jsx
+++
@@ -1,1 +0,0 @@
-const React = require("react");class Header extends React.Component {    constructor(props) {        super(props);    }    render() {        return <h2>{this.props.text}</h2>;    }}module.exports = Header;
